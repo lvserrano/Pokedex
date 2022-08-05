b3d850dbc521af76d0c2c13174d530d61d5a3685
levi serrano <lvserrano@outlook.com>
2022-08-05 09:06:26 -0300

    pegando o dado do input e fazendo o código funcionar

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 const pokemonName = document.querySelector('.pokemon__name');
 const pokemonNumber = document.querySelector('.pokemon__number');
 const pokemonImage= document.querySelector('.pokemon__image');
+const form= document.querySelector('.form');
+const input= document.querySelector('.input__search');
 
 
 const fetchPokemon = async (pokemon) => {
-  const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+  const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`);
   const data = await APIResponse.json();
   return data;
 }
@@ -14,6 +16,13 @@ const renderPokemon = async (pokemon) => {
   pokemonName.innerHTML = data['name'];
   pokemonNumber.innerHTML = data['id'];
   pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+  input.value = '';
 
 }
-renderPokemon('25')
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  renderPokemon(input.value);
+  
+} );
