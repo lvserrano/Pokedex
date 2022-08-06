e53491d1c9a5244bc461832224543feec5379ba6
levi serrano <lvserrano@outlook.com>
2022-08-06 18:17:29 -0300

    fazendo os botoes funcionarem

--- js/script.js
+++ js/script.js
@@ -4,9 +4,13 @@ const pokemonImage= document.querySelector('.pokemon__image');
 const form= document.querySelector('.form');
 const input= document.querySelector('.input__search');
 
+const buttonPrev= document.querySelector('.btn-prev');
+const buttonNext= document.querySelector('.btn-next');
+
+let searchPokemon = 1;
 
 const fetchPokemon = async (pokemon) => {
-  const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`);
+  const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
 
   if (APIResponse.status === 200){
   const data = await APIResponse.json();
@@ -14,7 +18,7 @@ const fetchPokemon = async (pokemon) => {
   }
 }
 const renderPokemon = async (pokemon) => {
-  pokemonName.innerHTML = 'Carregando...'
+ // pokemonName.innerHTML = 'Carregando...'
   pokemonNumber.innerHTML = '';
   const data = await fetchPokemon(pokemon);
   if(data){
@@ -22,17 +26,32 @@ const renderPokemon = async (pokemon) => {
     pokemonNumber.innerHTML = data['id'];
     pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
     input.value = '';
+    searchPokemon = data['id'];
   }else {
     pokemonName.innerHTML = 'Não encontrado :(';
     pokemonNumber.innerHTML = '';
     pokemonImage.src = '';
+
   }
 }
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
 
-  renderPokemon(input.value);
+  renderPokemon(input.value.toLowerCase());
   
 } );
-renderPokemon('1');
+buttonPrev.addEventListener('click', () => {
+
+if (searchPokemon > 1) {
+  searchPokemon -= 1;
+  renderPokemon(searchPokemon);
+}
+
+
+} );
+buttonNext.addEventListener('click', () => {
+  searchPokemon += 1;
+  renderPokemon(searchPokemon);
+} );
+renderPokemon(searchPokemon);
